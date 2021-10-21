ea51a33269c70f57f41e3261b6f06a1423dbb515
= <=>
2021-10-21 21:37:27 +0100

    Initial

---
+++ ReportFormatting/FormattingRibbon.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace ReportFormatting
+{
+    partial class FormattingRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public FormattingRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FormattingTab = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnSelectImages = this.Factory.CreateRibbonButton();
+            this.btnSelectFigures = this.Factory.CreateRibbonButton();
+            this.btnFormatLines = this.Factory.CreateRibbonButton();
+            this.FormattingTab.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FormattingTab
+            // 
+            this.FormattingTab.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.FormattingTab.Groups.Add(this.group1);
+            this.FormattingTab.Label = "Formatting";
+            this.FormattingTab.Name = "FormattingTab";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnSelectImages);
+            this.group1.Items.Add(this.btnSelectFigures);
+            this.group1.Items.Add(this.btnFormatLines);
+            this.group1.Name = "group1";
+            // 
+            // btnSelectImages
+            // 
+            this.btnSelectImages.Label = "Select Images";
+            this.btnSelectImages.Name = "btnSelectImages";
+            this.btnSelectImages.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSelectImages_Click);
+            // 
+            // btnSelectFigures
+            // 
+            this.btnSelectFigures.Label = "Select Figures";
+            this.btnSelectFigures.Name = "btnSelectFigures";
+            this.btnSelectFigures.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSelectFigures_Click);
+            // 
+            // btnFormatLines
+            // 
+            this.btnFormatLines.Label = "Format Lines";
+            this.btnFormatLines.Name = "btnFormatLines";
+            this.btnFormatLines.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnFormatLines_Click);
+            // 
+            // FormattingRibbon
+            // 
+            this.Name = "FormattingRibbon";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.FormattingTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.FormattingRibbon_Load);
+            this.FormattingTab.ResumeLayout(false);
+            this.FormattingTab.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab FormattingTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnSelectImages;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnSelectFigures;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnFormatLines;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal FormattingRibbon FormattingRibbon
+        {
+            get { return this.GetRibbon<FormattingRibbon>(); }
+        }
+    }
+}
---
+++ ReportFormatting/FormattingRibbon.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Word = Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+
+namespace ReportFormatting
+{
+    public partial class FormattingRibbon
+    {
+        private void FormattingRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnSelectImages_Click(object sender, RibbonControlEventArgs e)
+        {
+            //Init
+            Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+            Word.Application app = Globals.ThisAddIn.Application;
+            doc.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            //doc.Paragraphs.LineSpacing = 1.5f;
+
+            doc.Paragraphs.SpaceAfter = 0;
+            doc.Paragraphs.SpaceBefore = 0;
+            //doc.Paragraphs.SpaceAfterAuto = 0;
+            //doc.Paragraphs.SpaceBeforeAuto = 0;
+
+
+            //File dialog
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "image files (*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tif;*.tiff)|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tif;*.tiff";
+            fileDialog.Multiselect = true;
+            fileDialog.InitialDirectory = @"C:\";
+            var result = fileDialog.ShowDialog();
+
+            //Checking for files
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            //Insert all images
+            foreach (var sfileName in fileDialog.FileNames)
+            {
+                //Get Selection
+                Word.Range act_Image_Range = app.Selection.Range;
+
+                //Insert Image
+                Word.InlineShape inlineShape = doc.InlineShapes.AddPicture(sfileName, LinkToFile: false, SaveWithDocument: true, Range: act_Image_Range);
+
+                //scaling
+                inlineShape.LockAspectRatio = Microsoft.Office.Core.MsoTriState.msoTrue;
+                inlineShape.Height = app.CentimetersToPoints(10);
+
+                inlineShape.Line.Weight = 1;
+                inlineShape.Line.Visible = Microsoft.Office.Core.MsoTriState.msoTrue;
+            }
+        }
+
+        private void btnSelectFigures_Click(object sender, RibbonControlEventArgs e)
+        {
+            //Init
+            Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+            Word.Application app = Globals.ThisAddIn.Application;
+            doc.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+            //File dialog
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "image files (*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tif;*.tiff)|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tif;*.tiff";
+            fileDialog.Multiselect = true;
+            fileDialog.InitialDirectory = @"C:\";
+            var result = fileDialog.ShowDialog();
+
+            //Checking for files
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            //Insert all images
+            foreach (var sfileName in fileDialog.FileNames)
+            {
+                //Get Selection
+                Word.Range act_Image_Range = app.Selection.Range;
+
+                //Insert Image
+                Word.InlineShape inlineShape = doc.InlineShapes.AddPicture(sfileName, LinkToFile: false, SaveWithDocument: true, Range: act_Image_Range);
+
+                //scaling
+                inlineShape.LockAspectRatio = Microsoft.Office.Core.MsoTriState.msoTrue;
+                inlineShape.Height = app.CentimetersToPoints(10);
+
+                inlineShape.Line.Weight = 1.5f;
+                inlineShape.Line.Visible = Microsoft.Office.Core.MsoTriState.msoTrue;
+            }
+        }
+
+        private void btnFormatLines_Click(object sender, RibbonControlEventArgs e)
+        {
+            Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+            Word.Application app = Globals.ThisAddIn.Application;
+            doc.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            doc.Paragraphs.LineSpacingRule = Word.WdLineSpacing.wdLineSpace1pt5;
+            doc.Paragraphs.SpaceAfter = 0;
+            doc.Paragraphs.SpaceBefore = 0;
+            doc.Paragraphs.SpaceAfterAuto = 0;
+            doc.Paragraphs.SpaceBeforeAuto = 0;
+        }
+    }
+}
